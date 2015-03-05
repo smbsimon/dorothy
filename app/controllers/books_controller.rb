82196f4e85588d8c6726c5fb845a477979986f03
Sara Simon <smbsimon@gmail.com>
2015-03-05 14:22:45 -0500

    Change column in books table from author to author_id

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,51 @@
 class BooksController < ApplicationController
-  def index
+  def show
+    @book = Book.find(params[:id])
   end
+
+  def index
+    @books, @tag = Book.search_by_tag_name(params[:tag])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(books_params)
+    if @book.save
+      flash[:notice] = "Book was created."
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find params[:id]
+  end
+
+  def update
+    @book = Book.find params[:id]
+    if @book.update_attributes(book_params)
+      flash[:notice] = "Book was updated."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find params[:id]
+    book.destroy
+    flash[:notice] = "#{book} was destroyed."
+    redirect_to books_path
+  end
+
+  private
+
+  def article_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Author.generate_samples(10)
+Tag.generate_samples(10)
+Book.generate_samples(10)
